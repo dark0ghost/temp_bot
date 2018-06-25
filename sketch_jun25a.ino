ac5d95ae36cd670f34869531e2ed08212d96930e
dark0ghost <40036029+dark0ghost@users.noreply.github.com>
2018-06-25 20:28:41 +0300

    Add files via upload

---
+++ sketch_jun25a.ino
@@ -0,0 +1,36 @@
+#include <SoftwareSerial.h>
+#include <math.h>
+#include <SPI.h>
+
+
+#define TEMP_PIN  A4
+
+SoftwareSerial esp(4,5);
+
+
+void setup() {
+esp.begin(9600);
+String check; 
+String bot;
+
+}
+
+void loop() {
+String check;  
+while (check != "temp"){
+  check = Serial.read();
+  }
+if (check == "temp"){
+    temp();
+    }
+
+
+}
+void temp(){
+  float v_temp = 1023.0 / analogRead(TEMP_PIN)- 1.0;
+  float temp = 1.0 / (-log(v_temp)/3977.0 + 1.0 / 295.0) - 273.0;
+String  bot =  String (temp);
+  delay (100);
+  Serial.print(bot);
+  
+  }
